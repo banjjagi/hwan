0471ed9ffb5bb2bdbfac4a6703227713dcb47462
banjjagi <banjjagi@naver.com>
2015-07-11 10:42:14 +0900

    aaa

--- 자료모으기/이미지서치.ahk
+++
@@ -1,6 +0,0 @@
-﻿F1::
-ImageSearch,OutX,OutY,0,0,A_ScreenWidth,A_ScreenHeight, Image1.bmp
-If ErrorLevel = 0
-{
-	Mouseclick,OutX,OutY
-}
--- 자료모으기/파일통합하기.ps1
+++
@@ -1,3 +0,0 @@
-﻿cd D:\db\자료\작업중\백업
-get-childItem *.csv -Recurse | Rename-Item -NewName { $_.Name -replace ".csv", '.csev' }
-type *.csev >all.csv
